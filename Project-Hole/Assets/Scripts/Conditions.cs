a9b6c67151effca938f84d137cc57865acaf5478
Liam Liu <liu.liam@hotmail.com>
2021-01-09 14:11:36 -0500

    Gameplay
    Created the hole, increasing size, destroying objects, movement

---
+++ Project-Hole/Assets/Scripts/Conditions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditions : MonoBehaviour
+{
+    public int points = 0;
+    public OnChangePosition HoleScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        CalculateProgress();
+    }
+
+    private void CalculateProgress()
+    {
+        points++;
+
+        if(points % 10 == 0)
+        {
+            StartCoroutine(HoleScript.ScaleHole());
+        }
+    }
+}
